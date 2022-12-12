7a7c690ca77506a7978f4cdee568ef0bc3c00602
Filchev Igor <igor.filchev@zenitech.co.uk>
2022-12-12 05:48:48 +0200

    Login page was created

--- app/src/main/java/com/example/examapp/FirstFragment.java
+++ app/src/main/java/com/example/examapp/FirstFragment.java
@@ -47,6 +47,11 @@ public class FirstFragment extends Fragment {
 
         binding.helpPageButton.setOnClickListener(view1 -> {
             NavHostFragment.findNavController(FirstFragment.this)
+                    .navigate(R.id.action_FirstFragment_to_LoginFragment);
+        });
+
+        binding.helpPageButton.setOnClickListener(view1 -> {
+            NavHostFragment.findNavController(FirstFragment.this)
                     .navigate(R.id.action_FirstFragment_to_HelpPageFragment);
         });
     }
---
+++ app/src/main/java/com/example/examapp/LoginFragment.java
@@ -0,0 +1,42 @@
+package com.example.examapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.examapp.databinding.FragmentLoginBinding;
+
+public class LoginFragment extends Fragment {
+    private FragmentLoginBinding binding;
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.button2.setOnClickListener(view1 -> {
+
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
